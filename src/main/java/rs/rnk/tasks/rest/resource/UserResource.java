0f2ab17784ba21b8f444aef918c8f45dee6abc85
Milos Rankovic <milos@rnk.rs>
2019-10-07 18:31:25 +0200

    Change UserResource to use User model and return JSON

--- src/main/java/rs/rnk/tasks/rest/resource/UserResource.java
+++ src/main/java/rs/rnk/tasks/rest/resource/UserResource.java
@@ -1,18 +1,26 @@
 package rs.rnk.tasks.rest.resource;
 
+import rs.rnk.tasks.rest.model.User;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.Calendar;
+import java.util.Date;
 
 @Path("/users")
 public class UserResource {
 
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
+    @Produces(MediaType.APPLICATION_JSON)
     public Response sayHello() {
-        Response response = Response.ok("Hello World").build();
+        var birthDateCal = Calendar.getInstance();
+        birthDateCal.set(1990, Calendar.AUGUST, 12);
+
+        var user = new User("user", "user123", "user@test.com", "First Last", birthDateCal.getTime());
+        Response response = Response.ok(user).build();
 
         return response;
     }
