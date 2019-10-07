f8ae3d3b5732f6a88a1f9e85b10951c928053bb0
Milos Rankovic <milos@rnk.rs>
2019-10-07 18:00:14 +0200

    Initial commit

---
+++ src/main/java/rs/rnk/tasks/rest/TasksApiApplication.java
@@ -0,0 +1,13 @@
+package rs.rnk.tasks.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasksApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TasksApiApplication.class, args);
+	}
+
+}
