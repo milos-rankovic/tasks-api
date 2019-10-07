38b1a6d7418b984c708defcf78eac28808a7fc72
Milos Rankovic <milos@rnk.rs>
2019-10-07 19:01:13 +0200

    [refactor] Add /users/{id} mapping and use UserService for getting data

--- src/main/java/rs/rnk/tasks/rest/resource/UserResource.java
+++ src/main/java/rs/rnk/tasks/rest/resource/UserResource.java
@@ -1,28 +1,27 @@
 package rs.rnk.tasks.rest.resource;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import rs.rnk.tasks.rest.model.User;
-
+import rs.rnk.tasks.rest.service.UserService;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.util.Calendar;
-import java.util.Date;
 
 @Path("/users")
 public class UserResource {
 
+    @Autowired
+    private UserService userService;
+
     @GET
+    @Path("{id}")
     @Produces(MediaType.APPLICATION_JSON)
-    public Response sayHello() {
-        var birthDateCal = Calendar.getInstance();
-        birthDateCal.set(1990, Calendar.AUGUST, 12);
-
-        var user = new User("user", "user123", "user@test.com", "First Last", birthDateCal.getTime());
-        Response response = Response.ok(user).build();
-
-        return response;
+    public Response userById(@PathParam("id") int id) {
+        User user = userService.findById(id);
+        return Response.ok(user).build();
     }
 
 }
