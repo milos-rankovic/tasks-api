edccd2a63b464a74a481e616adad067881fb75ab
Milos Rankovic <milos@rnk.rs>
2019-10-07 18:15:38 +0200

    Create User model

---
+++ src/main/java/rs/rnk/tasks/rest/model/User.java
@@ -0,0 +1,69 @@
+package rs.rnk.tasks.rest.model;
+
+import java.util.Date;
+
+public class User {
+
+    private int id;
+    private String username;
+    private String password;
+    private String email;
+    private String name;
+    private Date birthDate;
+
+    public User(String username, String password, String email, String name, Date birthDate) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.name = name;
+        this.birthDate = birthDate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+}
