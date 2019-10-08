51a7d5665cfefb48ce70e29a13f131acf78d6bea
Milos Rankovic <milos@rnk.rs>
2019-10-08 12:16:46 +0200

    [refactor] Add list of tasks to user model

--- src/main/java/rs/rnk/tasks/rest/model/User.java
+++ src/main/java/rs/rnk/tasks/rest/model/User.java
@@ -3,6 +3,7 @@ package rs.rnk.tasks.rest.model;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import java.util.Date;
+import java.util.List;
 
 public class User {
 
@@ -16,16 +17,19 @@ public class User {
     @JsonIgnore
     private Date birthDate;
 
-    public User(String username, String password, String email, String name, Date birthDate) {
+    private List<Task> tasks;
+
+    public User(String username, String password, String email, String name, Date birthDate, List<Task> tasks) {
         this.username = username;
         this.password = password;
         this.email = email;
         this.name = name;
         this.birthDate = birthDate;
+        this.tasks = tasks;
     }
 
-    public User(int id, String username, String password, String email, String name, Date birthDate) {
-        this(username, password, email, name, birthDate);
+    public User(int id, String username, String password, String email, String name, Date birthDate, List<Task> tasks) {
+        this(username, password, email, name, birthDate, tasks);
         this.id = id;
     }
 
@@ -75,5 +79,13 @@ public class User {
 
     public void setBirthDate(Date birthDate) {
         this.birthDate = birthDate;
+    }
+
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
     }
 }
